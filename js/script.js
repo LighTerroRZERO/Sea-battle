104c7460a48a8210556cb2f5f62845c03d223ba5
LighTerroRZERO <lighterror1@mail.ru>
2020-03-06 12:33:39 +0300

    counter shot

--- js/script.js
+++ js/script.js
@@ -9,6 +9,46 @@ const play = {
   record: 0,
   shot: 0,
   hit: 0,
-  dead: 0
+  dead: 0,
+  set updateData(data) {
+    this[data]++;
+    this.render();
+  },
+  render() {
+    record.textContent = this.record;
+    shot.textContent = this.shot;
+    hit.textContent = this.hit;
+    dead.textContent = this.dead;
+  }
 }
 
+const show = {
+  hit() {
+
+  },
+
+  miss(elem) {
+    this.changeClass(elem, 'miss');
+  },
+
+  dead() {
+
+  },
+
+  changeClass(elem, value) {
+    elem.className = value;
+  }
+};
+
+
+const fire = (event) => {
+  const target = event.target;
+  show.miss(target);
+  play.updateData = 'shot';
+};
+
+const init = () => {
+  enemy.addEventListener('click', fire);
+};
+
+init();
